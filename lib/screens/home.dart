fc7b3305388cb25e2b9295265acf221296edf127
Priyank <er.priyank01@gmail.com>
2024-07-05 21:52:41 +0530

    location

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:priyank/constant_methods.dart';
 import 'package:restaurant_task/screens/login.dart';
 import 'package:restaurant_task/screens/profile.dart';
 
@@ -12,6 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   User? user;
+  Position? position;
 
   @override
   void initState() {
@@ -21,6 +25,33 @@ class _HomeScreenState extends State<HomeScreen> {
         user = FirebaseAuth.instance.currentUser;
       });
     });
+    _determinePosition();
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+    ConstantMethods().showLoaderDialog(context, Colors.purple, message: 'Fetch location');
+    position = await Geolocator.getCurrentPosition();
+    setState(() {});
+    Navigator.pop(context);
   }
 
   @override
@@ -45,7 +76,19 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: Center(
-        child: Text('Welcome ! ${user?.email}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome !'),
+            user != null ? Text('${user?.email}') : SizedBox(),
+            position != null ? Text('Latitude: ${position!.latitude}') : SizedBox(),
+            position != null ? Text('Longitude: ${position!.longitude}') : SizedBox(),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text('Note : Near restaurants list are not able to show because google place API require to payment to provide API KEY'),
+            ),
+          ],
+        ),
       ),
     );
   }
